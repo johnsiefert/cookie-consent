008dfe4665a4e45141f402ea134a959d3eed0e55
John <johnsief12@gmail.com>
2024-05-02 18:18:49 -0400

    added settimeout to pirate taking all your info

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-const modal = document.querySelector("#modal");
+const modal = document.querySelector('#modal');
 const closeBtn = document.querySelector('#modal-close-btn');
-const consent = document.querySelector("#consent-form");
-const modalText = document.querySelector("#modal-text");
-// const modalAccept = document.querySelector(".modal-btn");
+const consent = document.querySelector('#consent-form');
+const modalText = document.querySelector('#modal-text');
 
-setTimeout(function(){
-modal.style.display ="inline"
-}, 1500)
+setTimeout(function () {
+  modal.style.display = 'inline';
+}, 1500);
 
-function closeModal(){
-    modal.style.display ="none"
+function closeModal() {
+  modal.style.display = 'none';
 }
 
-function consentForm(e){
-e.preventDefault()
-modalText.innerHTML = `<div class="modal-inner-loading">
+function consentForm(e) {
+  e.preventDefault();
+  modalText.innerHTML = `<div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
     <p id="uploadText">
         Uploading your data to the dark web...
     </p>
 </div>`;
 
+setTimeout(function(){
+document.querySelector('#uploadText').innerText = `Making the sale...`;
+
+},1500)
+
+
+setTimeout(function(){
+    consent.style.display = "none"
+    document.querySelector(
+      '#modal-inner'
+    ).innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+},3000)
 }
 
-
-
-consent.addEventListener("submit", consentForm)
-closeBtn.addEventListener("click", closeModal)
+consent.addEventListener('submit', consentForm);
+closeBtn.addEventListener('click', closeModal);
