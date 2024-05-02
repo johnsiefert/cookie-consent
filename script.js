9e8f7a3ac657b4cddbd4e26ec2497359f769bf38
John <johnsief12@gmail.com>
2024-05-02 17:51:12 -0400

    added modal text when accept is clicked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector("#modal");
 const closeBtn = document.querySelector('#modal-close-btn');
 const consent = document.querySelector("#consent-form");
+const modalText = document.querySelector("#modal-text");
+// const modalAccept = document.querySelector(".modal-btn");
 
 setTimeout(function(){
 modal.style.display ="inline"
@@ -12,8 +14,16 @@ function closeModal(){
 
 function consentForm(e){
 e.preventDefault()
-console.log("form submitted")
+modalText.innerHTML = `<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
+
 }
+
+
 
 consent.addEventListener("submit", consentForm)
 closeBtn.addEventListener("click", closeModal)
